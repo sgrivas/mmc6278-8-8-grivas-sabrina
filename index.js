5bac4bf82bb7b078d5e3cf952a8d9e40858c4a3e
Sabrina Grivas <sgrivas1@gmail.com>
2022-06-30 14:12:10 -0400

    app behavior

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+var form=document.querySelector('#weather-app form')
+var weatherEl=document.getElementById('weather')
+
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather"
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    var userQuery=this.search.value
+    var queryString = "?units=imperial&appid=eaa52095952f3eaa180f48bf7902508c&q=" + userQuery
+    var fetchURL = weatherUrl + queryString
+    fetch(fetchURL)
+    .then(function(res){
+        return res.json()
+    })
+    .then(renderedCity)
+})
+
+function renderedCity(city){
+    weatherEl.textContent=''
+
+    var h2=document.createElement('h2')
+    h2.textContent=city.name+", "+city.sys.country
+    weatherEl.appendChild(h2)
+
+    var map=document.createElement('a')
+    map.href=`https://www.google.com/maps/search/?api=1&query=${city.coord.lat},${city.coord.lon}`
+    map.target="_BLANK"
+    map.textContent="Click to view map"
+    weatherEl.appendChild(map)
+
+    var img=document.createElement('img')
+    img.src=`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`
+    weatherEl.appendChild(img)
+
+    var condition=document.createElement('p')
+    condition.textContent=city.weather[0].description
+    condition.style="text-transform: capitalize"
+    weatherEl.appendChild(condition)
+
+    var temp=document.createElement('p')
+    temp.textContent=`Current: ${city.main.temp}° F`
+    weatherEl.appendChild(temp)
+
+    var feelslike=document.createElement('p')
+    feelslike.textContent=`Feels like: ${city.main.feels_like}° F`
+    weatherEl.appendChild(feelslike)
+
+    var milliTime=(city.dt)*1000
+    var date = new Date(milliTime)
+    var timeString = date.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit'
+    })
+    var lastUpdated=document.createElement('p')
+    lastUpdated.textContent=`Last updated: ${timeString}`
+    weatherEl.appendChild(lastUpdated)
+}
